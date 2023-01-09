7e0617a88df10c783da6c2b43733ba18f455677a
NPM8 <nnnp.dawid@gmail.com>
2023-01-09 19:22:03 +0000

    Deploying to gh-pages from @ NPM8/goit-group-9@f66bc0d84d12b36e4dfc9f3ec8099c55168c51c1 🚀

---
+++ class_2/index.21803ab7.js
@@ -0,0 +1,2 @@
+const SUNNY_WEATHER="sunny",RAINY_WEATHER="rainy",STORMY_WEATHER="stormy",weather="stormy";switch(weather){case"stormy":console.log("Kalosze");break;case"rainy":console.log("Płaszcz");case"sunny":console.log("Dlugie spodnie i t-shirt");default:console.log("Bielizna")}const TEXT="ala ma kota, kot ma ale";
+//# sourceMappingURL=index.21803ab7.js.map
